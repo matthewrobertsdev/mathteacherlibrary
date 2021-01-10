a309ac8ed81d6d78acf9d12b31bb508f3cb07aca
matthewrobertsdev <31932179+matthewrobertsdev@users.noreply.github.com>
2021-01-10 00:25:24 -0500

    export getRandomIntArray now

--- utilities/GetRandomInt.js
+++ utilities/GetRandomInt.js
@@ -1,3 +1,11 @@
 export const getRandomInt=function getRandomInt(min, max) {
   return Math.floor(Math.random()*(max-min+1))-min
 }
+
+export const getRandomIntArray=function getRandomIntArray(num, min, max) {
+  const array=[]
+  for (let i=0; i<num; i++) {
+    array.push(getRandomInt(min, max))
+  }
+  return array
+}
